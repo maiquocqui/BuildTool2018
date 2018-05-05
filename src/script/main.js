6159fe49b302400527e32904bf0c22f294d300d2
maiquocqui <maiquocqui@gmail.com>
2018-05-05 11:50:49 +0700

    edit script info
    ...

--- src/script/main.js
+++ src/script/main.js
@@ -1,3 +1,3 @@
 $(document).ready(function() {
-    $('main').html("<h1 class='alert alert success'>test script reload successful</h1>");
+    $('main').html("<h1 class='alert alert info'>test script reload info</h1>");
 });
