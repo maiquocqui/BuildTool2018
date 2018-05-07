db6c5e5ef125b6d3d699482227e8b842987a1869
maiquocqui <maiquocqui@gmail.com>
2018-05-07 11:44:41 +0700

    edit
    ...

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,3 @@
-"use strict";
-
-$(document).ready(function () {
+$(document).ready(function() {
     $('main').html("<div class='container'><h1 class='alert alert-danger'>test script reload info</h1></div>");
 });
